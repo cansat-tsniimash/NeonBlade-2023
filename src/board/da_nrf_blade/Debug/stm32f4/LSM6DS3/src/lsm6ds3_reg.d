f2b1894e467e756e78588c7e64d30862b390721e
@man-GO-228 <t.ozhigow@gmail.com>
2023-03-04 16:07:10 +0300

    Делаем уборку

--- src/board/da_nrf_blade/Debug/stm32f4/LSM6DS3/src/lsm6ds3_reg.d
+++
@@ -1,5 +0,0 @@
-stm32f4/LSM6DS3/src/lsm6ds3_reg.o: \
- X:/Cansat\ 2023/Neon\ Blade/Software/lib-tsniimash/stm32f4/LSM6DS3/src/lsm6ds3_reg.c \
- x:\cansat\ 2023\neon\ blade\software\lib-tsniimash\stm32f4\lsm6ds3\lsm6ds3_reg.h
-
-x:\cansat\ 2023\neon\ blade\software\lib-tsniimash\stm32f4\lsm6ds3\lsm6ds3_reg.h:
